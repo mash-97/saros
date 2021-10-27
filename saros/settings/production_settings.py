9bf82960deeca8728b1471bf42dfa89a8cc81c5e
mash-97 <mahimnhd97@gmail.com>
2021-10-27 12:54:49 +0600

    product

--- saros/settings/production_settings.py
+++ saros/settings/production_settings.py
@@ -2,5 +2,17 @@ from saros.settings.shared_settings import *
 
 DEBUG = False 
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
-
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
